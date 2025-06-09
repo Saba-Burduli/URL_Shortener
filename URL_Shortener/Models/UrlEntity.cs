ce1dd7d23b44e5bc05a6769e365a768e918fdffb
SabaBurduli888 <sabagg790@gmail.com>
2025-06-09 21:03:19 +0400

    adding in UrlEntity.cs some properties

--- URL_Shortener/Models/UrlEntity.cs
+++ URL_Shortener/Models/UrlEntity.cs
@@ -2,5 +2,16 @@ namespace URL_Shortener.Models;
 
 public class UrlEntity
 {
+    public string ShortCode { get; set; }
+
+    public string OriginalUrl { get; set; }
+
+    public DateTime? CreatedAt { get; set; } = DateTime.Now;
+
+    public DateTime? ExpirationDate { get; set; } = DateTime.Now;
+
+    public int ClickCount { get; set; }
+
+    public bool IsActive { get; set; }
     
 }
