682d1b35fe378c76b345726170b69bc3dc9c4f83
Saba-Burduli <sabagg790@gmail.com>
2025-06-10 01:27:31 +0400

    Add background execution to ExpirationService
    Implemented the `BackgroundService` base class for `ExpirationService` to enable background task processing. Integrated `ICassandraSessionFactory` for database handling and

--- URL_Shortener/Services/ExpirationService.cs
+++ URL_Shortener/Services/ExpirationService.cs
@@ -1,6 +1,20 @@
+using URL_Shortener.InterFaces;
+
 namespace URL_Shortener.Services;
 
-public class ExpirationService
+public class ExpirationService : BackgroundService
 {
-    
+    private readonly ICassandraSessionFactory _factory;
+
+    public ExpirationService(ICassandraSessionFactory factory)
+    {
+        _factory = factory;
+    }
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        if (!stoppingToken.IsCancellationRequested)
+        {
+            
+        }
+    }
 }
