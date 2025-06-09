ba90d15a4aa35ceed42bf6fb416e606d966dfa6d
Saba-Burduli <sabagg790@gmail.com>
2025-06-10 02:21:42 +0400

    Enable essential services and dependencies in Program.cs
    Re-enabled Swagger generation, hosted ExpirationService, and singleton services for URL handling and Cassandra session management. These additions ensure proper API documentation, background service execution, and dependency injection for core functionalities.

--- URL_Shortener/Program.cs
+++ URL_Shortener/Program.cs
@@ -1,12 +1,15 @@
+using URL_Shortener.InterFaces;
+using URL_Shortener.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddAuthentication();
 builder.Services.AddAuthorization();
 builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
-//builder.Services.AddHostedService<ExpirationService>();
-//builder.Services.AddSingleton<IUrlService, UrlService>();
-//builder.Services.AddSingleton<ICassandraSessionFactory, CassandraSessionFactory>();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHostedService<ExpirationService>();
+builder.Services.AddSingleton<IUrlService, UrlService>();
+builder.Services.AddSingleton<ICassandraSessionFactory,>();
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
