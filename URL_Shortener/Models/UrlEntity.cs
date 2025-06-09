203d844a1a1d293005d0d2cbb7ca0620d6a6ac22
Saba-Burduli <sabagg790@gmail.com>
2025-06-10 01:28:02 +0400

    Make UrlEntity properties nullable
    Updated the UrlEntity model to make its properties nullable, ensuring better handling of optional or missing data. This change enhances flexibility in scenarios where property values may not always be available.

--- URL_Shortener/Models/UrlEntity.cs
+++ URL_Shortener/Models/UrlEntity.cs
@@ -2,16 +2,16 @@ namespace URL_Shortener.Models;
 
 public class UrlEntity
 {
-    public string ShortCode { get; set; }
+    public string? ShortCode { get; set; }
 
-    public string OriginalUrl { get; set; }
+    public string? OriginalUrl { get; set; }
 
     public DateTime? CreatedAt { get; set; } = DateTime.Now;
 
     public DateTime? ExpirationDate { get; set; } = DateTime.Now;
 
-    public int ClickCount { get; set; }
+    public int? ClickCount { get; set; }
 
-    public bool IsActive { get; set; }
+    public bool? IsActive { get; set; }
     
 }
