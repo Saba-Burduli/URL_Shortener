0d89067f11dc62deabc065cd0ba2f594d9f3cd87
Saba-Burduli <sabagg790@gmail.com>
2025-06-10 01:27:17 +0400

    Make properties in AnalyticsEntity nullable
    Changed ShortCode, ClickDate, UserAgent, and IpAddress to nullable types. This ensures better handling of missing or optional data, reducing potential null reference issues during runtime.

--- URL_Shortener/Models/AnalyticsEntity.cs
+++ URL_Shortener/Models/AnalyticsEntity.cs
@@ -2,13 +2,13 @@ namespace URL_Shortener.Models;
 
 public class AnalyticsEntity
 {
-    public string ShortCode { get; set; }
+    public string? ShortCode { get; set; }
     
-    public string ClickDate { get; set; }
+    public string? ClickDate { get; set; }
 
-    public string UserAgent { get; set; }
+    public string? UserAgent { get; set; }
 
-    public string IpAddress { get; set; }
+    public string? IpAddress { get; set; }
 
     //string ShortCode
     // 
