b2a0e9f6a9682d9fe91885663b1e1298ffd4db5f
SabaBurduli888 <sabagg790@gmail.com>
2025-06-09 21:04:10 +0400

    implementing methods from IUrlService without any Service logic

--- URL_Shortener/Services/UrlService.cs
+++ URL_Shortener/Services/UrlService.cs
@@ -1,6 +1,32 @@
+using URL_Shortener.InterFaces;
+using URL_Shortener.Models;
+
 namespace URL_Shortener.Services;
 
-public class UrlService
-{
-    
+public class UrlService : IUrlService
+{ 
+    public Task<string> RedirectAsync(string code, string userAgent, string ip)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> CreateUrlAsync(CreateUrlRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UrlEntity> GetUrlDetailsAsync(string code)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UrlEntity> UpdateUrlAsync(string code, UpdateUrlRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DelateUrlAsync(string code)
+    {
+        throw new NotImplementedException();
+    }
 }
