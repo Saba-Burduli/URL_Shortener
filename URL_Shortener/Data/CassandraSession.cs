a6619933b3810be36df9b3072d6f16f054e715c9
Saba-Burduli <sabagg790@gmail.com>
2025-06-10 03:20:54 +0400

    Refactor Cassandra session methods and fix binding issues
    Revised `InsertUrlAsync` and `GetUrlAsync` methods to include `SimpleStatement` for binding query parameters. Updated `DeactivateExpiredUrlsAsync` to accept `SimpleStatement`, resolving parameter handling issues. This improves functionality and query execution consistency.

--- URL_Shortener/Data/CassandraSession.cs
+++ URL_Shortener/Data/CassandraSession.cs
@@ -1,23 +1,31 @@
+using Cassandra;
 using URL_Shortener.InterFaces;
 using URL_Shortener.Models;
+using CassandraSession = Cassandra.ISession;
 
 namespace URL_Shortener.Data;
 
 public class CassandraSession : ICassandraSession
     {
-        private readonly ISession _session;
-        public CassandraSession(ISession session)
+        private readonly CassandraSession _session;
+        public CassandraSession(CassandraSession session)
         {
             _session = session;
         }
-        public Task<UrlEntity> InsertUrlAsync(UrlEntity url)
-        {
-            throw new NotImplementedException();
-        }
+        
+            public async Task<UrlEntity> InsertUrlAsync(UrlEntity url)
+            {
+                var st = new SimpleStatement(
+                    "INSERT INTO urls (shortcode, originalurl, createdat, expirationdate, clickcount, isactive) VALUES (?, ?, ?, ?, ?, ?)");
+                var bs = st.Bind(url.ShortCode, url.OriginalUrl, url.CreatedAt, url.ExpirationDate, url.ClickCount, url.IsActive); 
+                await _session.DeactivateExpiredUrlsAsync(bs); // added insise invoked method (SimpleStatment bs) parameter
+                return url;
+            }
 
-        public Task<UrlEntity> GetUrlAsync(string code)
+        public async Task<UrlEntity> GetUrlAsync(string code)
         {
-            throw new NotImplementedException();
+            var st = new SimpleStatement("SELECT * FROM urls WHERE shortcode = ?", code);
+            var row = (await _session.DeactivateExpiredUrlsAsync(st)).FirstOrDefaultAsync();
         }
 
         public Task<UrlEntity> UpdateUrlAsync(string code, UpdateUrlRequest request)
@@ -40,7 +48,7 @@ public class CassandraSession : ICassandraSession
             throw new NotImplementedException();
         }
 
-        public Task DeactivateExpiredUrlsAsync()
+        public Task DeactivateExpiredUrlsAsync(SimpleStatement bs) // i added simpleStatment for fix error in line 21
         {
             throw new NotImplementedException();
         }
