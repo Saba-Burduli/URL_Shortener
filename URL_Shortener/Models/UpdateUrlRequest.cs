0018d82fdc6cc4e4486d05b398baa1be1c7a6ad4
SabaBurduli888 <sabagg790@gmail.com>
2025-06-09 21:03:03 +0400

    Adding some properties in UpdateUrlRequest.cs

--- URL_Shortener/Models/UpdateUrlRequest.cs
+++ URL_Shortener/Models/UpdateUrlRequest.cs
@@ -2,5 +2,7 @@ namespace URL_Shortener.Models;
 
 public class UpdateUrlRequest
 {
+    public string OriginalUrl { get; set; }
     
+    public DateTime ExpirationDate { get; set; } = DateTime.Now;
 }
