38bedb7084794fb91933d2b7dd34917fc0353b77
SabaBurduli888 <sabagg790@gmail.com>
2025-06-09 21:01:09 +0400

    adding properties in CreateUrlRequest.cs

--- URL_Shortener/Models/CreateUrlRequest.cs
+++ URL_Shortener/Models/CreateUrlRequest.cs
@@ -2,5 +2,10 @@ namespace URL_Shortener.Models;
 
 public class CreateUrlRequest
 {
+    public string OriginalUrl { get; set; }
     
+    public string CustomAlias { get; set; }
+
+    public DateTime ExpirationDate { get; set; } = DateTime.Now;
+
 }
