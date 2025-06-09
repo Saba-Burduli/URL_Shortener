aad97078763a9b6e98c26b3fba33f32b60f1d7be
SabaBurduli888 <sabagg790@gmail.com>
2025-06-09 20:35:07 +0400

    adding properties in AnalyticsEntity.cs

--- URL_Shortener/Models/AnalyticsEntity.cs
+++ URL_Shortener/Models/AnalyticsEntity.cs
@@ -2,5 +2,19 @@ namespace URL_Shortener.Models;
 
 public class AnalyticsEntity
 {
+    public string ShortCode { get; set; }
     
+    public string ClickDate { get; set; }
+
+    public string UserAgent { get; set; }
+
+    public string IpAddress { get; set; }
+
+    //string ShortCode
+    // 
+    // DateTime ClickDate
+    // 
+    // string UserAgent
+    // 
+    // string IpAddress
 }
