499d50e0b8b0b78e0a86d5bff04e58067523e52d
Saba-Burduli <sabagg790@gmail.com>
2025-06-10 01:27:24 +0400

    Make properties in CreateUrlRequest nullable
    Changed `OriginalUrl`, `CustomAlias`, and `ExpirationDate` to nullable types to handle optional values more flexibly. This ensures better support for use cases where some fields may not always be provided.

--- URL_Shortener/Models/CreateUrlRequest.cs
+++ URL_Shortener/Models/CreateUrlRequest.cs
@@ -2,10 +2,10 @@ namespace URL_Shortener.Models;
 
 public class CreateUrlRequest
 {
-    public string OriginalUrl { get; set; }
+    public string? OriginalUrl { get; set; }
     
-    public string CustomAlias { get; set; }
+    public string? CustomAlias { get; set; }
 
-    public DateTime ExpirationDate { get; set; } = DateTime.Now;
+    public DateTime? ExpirationDate { get; set; } = DateTime.Now;
 
 }
