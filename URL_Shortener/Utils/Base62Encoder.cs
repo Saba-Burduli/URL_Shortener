c27de31e839d40c7ef5bbae942d564c4e8fce39e
Saba-Burduli <sabagg790@gmail.com>
2025-06-10 02:21:18 +0400

    Add Base62 encoding logic to Base62Encoder class
    Implemented the `Encode` method to convert a long number into a Base62 string using a defined alphabet. This functionality is essential for creating short and unique IDs in the URL shortener.

--- URL_Shortener/Utils/Base62Encoder.cs
+++ URL_Shortener/Utils/Base62Encoder.cs
@@ -1,6 +1,19 @@
+using System.Text;
+
 namespace URL_Shortener.Utils;
 
 public class Base62Encoder
 {
-    
+    private const string Alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    public static string Encode(long num)
+    {
+        var Enc = new StringBuilder();
+        while (num > 0)
+        {
+            Enc.Insert(0, Alphabet[(int)(num % 62)]);
+            num /= 62;
+        }
+        
+        return Enc.ToString();
+    }
 }
