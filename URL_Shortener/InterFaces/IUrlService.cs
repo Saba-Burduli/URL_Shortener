94b07f3ca9ebaf67e5c8e15fc7659f0116253e34
SabaBurduli888 <sabagg790@gmail.com>
2025-06-09 21:02:44 +0400

    Adding Methods for IUrlService.cs

--- URL_Shortener/InterFaces/IUrlService.cs
+++ URL_Shortener/InterFaces/IUrlService.cs
@@ -1,6 +1,12 @@
+using URL_Shortener.Models;
+
 namespace URL_Shortener.InterFaces;
 
 public interface IUrlService
 {
-    
+    Task<string> RedirectAsync(string code, string userAgent, string ip);
+    Task<string> CreateUrlAsync(CreateUrlRequest request);
+    Task<UrlEntity> GetUrlDetailsAsync(string code);
+    Task<UrlEntity> UpdateUrlAsync(string code, UpdateUrlRequest request);
+    Task<bool> DelateUrlAsync(string code);
 }
